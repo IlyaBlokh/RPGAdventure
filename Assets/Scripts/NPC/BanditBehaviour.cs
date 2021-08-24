f22f825ee24e409aabaa7d849fb473396c5eb6a7
Ilya Blokh <ilyablokh@gmail.com>
2021-08-24 17:13:47 +0500

    enemy detection range

--- Assets/Scripts/NPC/BanditBehaviour.cs
+++ Assets/Scripts/NPC/BanditBehaviour.cs
@@ -6,9 +6,32 @@ namespace RPGAdventure
 {
     public class BanditBehaviour : MonoBehaviour
     {
-        private void Start()
-        {
+        [SerializeField]
+        float DetectionRange = 10.0f;
 
+        private float m_CurrentDistance;
+
+        private void Update()
+        {
+            LookForPlayer();
+        }
+
+        private PlayerController LookForPlayer()
+        {
+            if (PlayerController.Instance == null)
+            {
+                return null;
+            }
+
+            m_CurrentDistance = (PlayerController.Instance.transform.position - transform.position).magnitude;
+            if (m_CurrentDistance <= DetectionRange)
+            {
+                return PlayerController.Instance;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
