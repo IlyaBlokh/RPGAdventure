804a86543debba4a272977a49e210d729b496983
Ilya Blokh <ilyablokh@gmail.com>
2021-08-26 17:36:17 +0500

    add nav mesh, stopping pursuit logic

--- Assets/Scripts/NPC/BanditBehaviour.cs
+++ Assets/Scripts/NPC/BanditBehaviour.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 namespace RPGAdventure
@@ -10,13 +9,45 @@ namespace RPGAdventure
         float DetectionRange = 10.0f;
         [SerializeField]
         float DetectionAngle = 90.0f;
+        [SerializeField]
+        float TimeToStopPursuit = 2.0f;
+
+
+        private NavMeshAgent m_NavMeshAgent;
 
         private float m_DistanceToPlayer;
         private Vector3 m_LookAtPlayer;
+        private PlayerController m_Target;
+        private float m_TimeNoDetecting;
+
+        private void Awake()
+        {
+            m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        }
 
         private void Update()
         {
-            LookForPlayer();
+            var target = LookForPlayer();
+            if (m_Target == null)
+            {
+                if (target != null)
+                {
+                    m_Target = target;
+                }
+            }
+            else
+            {
+                m_NavMeshAgent.SetDestination(m_Target.transform.position);
+                if (target == null)
+                {
+                    m_TimeNoDetecting += Time.deltaTime;
+                    if (m_TimeNoDetecting > TimeToStopPursuit)
+                    {
+                        m_Target = null;
+                        m_TimeNoDetecting = .0f;
+                    }
+                }
+            }
         }
 
         private PlayerController LookForPlayer()
@@ -30,7 +61,7 @@ namespace RPGAdventure
             m_DistanceToPlayer = m_LookAtPlayer.magnitude;
             if (m_DistanceToPlayer <= DetectionRange)
             {
-                if (Vector3.Dot(transform.forward, m_LookAtPlayer) > Mathf.Cos(Mathf.Deg2Rad * DetectionAngle / 2))
+                if (Vector3.Dot(transform.forward, m_LookAtPlayer.normalized) > Mathf.Cos(Mathf.Deg2Rad * DetectionAngle / 2))
                 {
                     return PlayerController.Instance;
                 }
