8c4cd8c32ffcb97dd81f610aa239b35acc0a2de5
Ilya Blokh <ilyablokh@gmail.com>
2021-10-15 16:46:21 +0500

    add attacking points to weapon

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -6,12 +6,37 @@ namespace RPGAdventure
 {
     public class MeleeWeapon : MonoBehaviour
     {
+        [System.Serializable]
+        public class AttackPoint
+        {
+            public float radius;
+            public Vector3 offset;
+            public Transform root;
+        }
+
         [SerializeField]
         float Damage;
+
+        [SerializeField]
+        AttackPoint[] attackPoints = new AttackPoint[0];
 
         public void Attack()
         {
             Debug.Log("Melee weapon is swinging");
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = new Color(.0f, 0.7f, .0f, 0.4f);
+            foreach(AttackPoint attackPoint in attackPoints)
+            {
+                var worldOffset = attackPoint.root.TransformVector(attackPoint.offset);
+                Gizmos.DrawSphere(
+                    transform.position + worldOffset,
+                    attackPoint.radius);
+            }
+        }
+#endif
     }
 }
