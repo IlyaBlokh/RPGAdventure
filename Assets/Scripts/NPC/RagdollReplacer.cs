a6c919e0c7a71e921246ce1457d2e155cc79dafe
Ilya Blokh <ilyablokh@gmail.com>
2021-10-20 17:30:09 +0500

    dead enemy vfx

--- Assets/Scripts/NPC/RagdollReplacer.cs
+++ Assets/Scripts/NPC/RagdollReplacer.cs
@@ -7,10 +7,18 @@ namespace RPGAdventure {
     {
         [SerializeField]
         GameObject RagdollPrefab;
+
+        [SerializeField]
+        GameObject DeadVFX;
         public void ReplaceWithRagdoll()
         {
             GameObject ragdoll = Instantiate(RagdollPrefab, transform.position, transform.rotation);
             CopyTransform(transform, ragdoll.transform);
+
+            GameObject deadVFX = Instantiate(DeadVFX);
+            deadVFX.transform.transform.position = ragdoll.transform.position;
+            deadVFX.transform.parent = ragdoll.transform;
+
             Destroy(gameObject);
         }
 
