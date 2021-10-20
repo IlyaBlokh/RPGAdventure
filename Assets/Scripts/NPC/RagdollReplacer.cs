c9566344e96ddc911a67240edd715c7c7b959403
Ilya Blokh <ilyablokh@gmail.com>
2021-10-20 16:11:56 +0500

    update ragdoll transform

--- Assets/Scripts/NPC/RagdollReplacer.cs
+++ Assets/Scripts/NPC/RagdollReplacer.cs
@@ -10,8 +10,18 @@ namespace RPGAdventure {
         public void ReplaceWithRagdoll()
         {
             GameObject ragdoll = Instantiate(RagdollPrefab, transform.position, transform.rotation);
-
+            CopyTransform(transform, ragdoll.transform);
             Destroy(gameObject);
+        }
+
+        public void CopyTransform(Transform original, Transform ragdoll)
+        {
+            ragdoll.position = original.position;
+            ragdoll.rotation = original.rotation;
+            for (var i = 0; i < original.childCount; i++)
+            {
+                CopyTransform(original.GetChild(i), ragdoll.GetChild(i));
+            }
         }
     }
 }
