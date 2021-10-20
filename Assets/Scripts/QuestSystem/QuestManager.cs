9ee5887a0a814442b11b5c0efa6e75ce7654e472
Ilya Blokh <ilyablokh@gmail.com>
2021-10-20 21:30:07 +0500

    quest and questmanager classes

---
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGAdventure
+{
+    public class QuestManager : MonoBehaviour
+    {
+        [SerializeField]
+        List<Quest> Quests;
+    }
+}
