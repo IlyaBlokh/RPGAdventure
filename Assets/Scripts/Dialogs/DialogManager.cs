948a2165dba3f1a1cc797e466dfeb47ca77dc942
Ilya Blokh <ilyablokh@gmail.com>
2021-10-27 17:38:28 +0500

    show dialog UI

--- Assets/Scripts/Dialogs/DialogManager.cs
+++ Assets/Scripts/Dialogs/DialogManager.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPGAdventure
 {
     public class DialogManager : MonoBehaviour
     {
+        [SerializeField]
+        GameObject DialogUI;
+        [SerializeField]
+        Text DialogHeaderText;
+
+        private void Awake()
+        {
+            DialogUI.SetActive(false);
+        }
         private void Update()
         {
             if (PlayerInput.Instance != null && PlayerInput.Instance.IsInteracking)
             {
-                Debug.Log("Start interact");
+                StartDialog();
             }
+        }
+
+        private void StartDialog()
+        {
+            DialogUI.SetActive(true);
+            DialogHeaderText.text = "TEST";
         }
     }
 }
